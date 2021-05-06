f460b8ce88a80203b51cfc867f56ae9d4de98e6c
epicmario7133 <56398081+epicmario7133@users.noreply.github.com>
2021-05-06 19:54:24 +0200

    Create HW-494-reader.py

---
+++ HW-494/HW-494-reader.py
@@ -0,0 +1,12 @@
+import machine
+import utime
+
+analog_value = machine.ADC(28)
+
+while True:
+    reading = analog_value.read_u16()
+    if reading < 50000:
+        print("The sensor has been touched")
+    else:
+        print("The sensor was not touched")
+    utime.sleep(1.0)
