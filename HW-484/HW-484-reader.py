1c5029bf02d407bdc155ce759bb892f6900a83ab
epicmario7133 <56398081+epicmario7133@users.noreply.github.com>
2021-05-06 19:40:41 +0200

    Create HW-484-reader.py

---
+++ HW-484/HW-484-reader.py
@@ -0,0 +1,12 @@
+import machine
+import utime
+
+analog_value = machine.ADC(28)
+
+while True:
+    reading = analog_value.read_u16()
+    if reading < 1000:
+        print("Magnet detected")
+    else:
+        print("Magnet not detected")
+    utime.sleep(1.0)
