65806f3f2b2a24aa854e882700451e6d255399a1
epicmario7133 <56398081+epicmario7133@users.noreply.github.com>
2021-05-06 22:32:24 +0200

    Create HW-501-reader.py

---
+++ HW-501/HW-501-reader.py
@@ -0,0 +1,10 @@
+from machine import Pin
+import utime
+
+sensor = Pin(16, Pin.IN, Pin.PULL_UP)
+while True:
+    if sensor.value() == 0:
+        print("The sensor found inclination")
+    else:
+        print("The sensor did not find inclination")
+    utime.sleep(1)
