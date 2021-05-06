fad2b4efe8502dc1155a949135efb4fc705402cc
epicmario7133 <56398081+epicmario7133@users.noreply.github.com>
2021-05-06 22:08:44 +0200

    Create HW-511-reader.py

---
+++ HW-511/HW-511-reader.py
@@ -0,0 +1,10 @@
+from machine import Pin
+import utime
+
+sensor = Pin(16, Pin.IN, Pin.PULL_UP)
+while True:
+    if sensor.value() == 0:
+        print("The sensor found something")
+    else:
+        print("The sensor did not find anything")
+    utime.sleep(1)
